1a6d0c84aa38dbee00b1b6adf525ccc19027c49b
JuanP18 <jp_1805@hotmail.com>
2018-03-26 00:16:54 -0500

    Prueba ---------
    Probando github para no hacer una Fernandada

---
+++ resilient/app/Http/Controllers/UsersController.php
@@ -0,0 +1,87 @@
+<?php
+// aqui va la logica de la aplicacion 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User; // modelo representacion de la base de datos
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all(); // Consulta SQL eloquent consultas JPA 
+        //users contiene todos los usuarios que estan en la tabla usuarios 
+        return view('reporteusuarios')->with('users',$users); //retorna un json 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
