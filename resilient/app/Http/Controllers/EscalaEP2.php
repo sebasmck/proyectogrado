e162f998e25d3b81e634cf04b475442a10423999
JuanP18 <jp_1805@hotmail.com>
2018-08-07 15:33:40 -0500

    Escala EP2
    models
    y controller (falta hacerlo bien)

---
+++ resilient/app/Http/Controllers/EscalaEP2.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\EscalaEP2Acudiente;
+use App\EscalaResilienciaEP2;
+use App\RespuestaEP2;
+use App\Cuidador; 
+
+class EscalaEP2 extends Controller
+{
+     public function index()
+    {
+        return null; 
+    }
+
+     public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, $id_usuario, $id_respuesta)
+    {
+         $interseccion = new EscalaEP2Acudiente();
+         $escala = new EscalaResilienciaEP2();
+
+
+         
+          // datos escala puntajes 1,2,3,4 
+          
+          // datos para la intersección 
+          $inter-> = $inter->;
+          $inter-> = $inter->;
+
+          dd($request); 
+    }
+    public function 
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+}
---
+++ resilient/app/RespuestaEP2.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RespuestaEP2 extends Model
+{
+    protected $table = 'respuesta_ep2';
+
+    protected $primaryKey = 'Id_Respuesta';
+
+    public $timestamps = false;
+}
