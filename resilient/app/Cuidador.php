a9f89ec18e5616cf094ca663444375afaed70d03
melomelos <dfmm96@hotmail.com>
2018-08-14 23:05:39 -0500

    Cuidador
    Commit de Cuidador.php

--- resilient/app/Cuidador.php
+++ resilient/app/Cuidador.php
@@ -12,12 +12,23 @@ class Cuidador extends Model
 
     public $timestamps = false;
 
+
     public function escolaridad(){
        return $this->hasone('App\Escolaridad' , 'Id_Escolaridad' , 'Id_Escolaridad');
     }
 
     public function estadoCivil(){
         return $this->hasone('App\EstadoCivil' , 'Id_EstadoCivil' , 'Id_EstadoCivil');
+    }
+
+
+    public function sexo(){
+        return $this->BelongsTo('App\Sexo','Id_Sexo');
+    }
+
+
+    public function ocupacion(){
+        return $this->belongsTo('App\Ocupacion','Id_Ocupacion');
     }
 
 
