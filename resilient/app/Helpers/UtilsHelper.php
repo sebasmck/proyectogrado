0a5b3db6bfd352044c72470fbd1e2218b79a5c5c
Unknown <hardmattlake@hotmail.com>
2018-09-15 21:44:55 -0500

    Modelos Actividad
    Se realizaron los modelos correspondientes al módulo de actividades

---
+++ resilient/app/Helpers/UtilsHelper.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andres Muñoz
+ * Date: 15/09/2018
+ * Time: 08:21 PM
+ */
+
+namespace App\Helpers;
+
+
+class UtilsHelper
+{
+    function ok() {
+
+    }
+}
---
+++ resilient/app/PreguntaActividades.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PreguntaActividades extends Model
+{
+    protected $table = "pregunta_actividades";
+
+    protected $primaryKey = "Id_Pregunta";
+
+    protected $timestamps = false;
+
+    public function opcionPreguntaActividad(){
+        return $this->hasMany('App/OpcionPreguntaActividad','Id_Pregunta');
+    }
+
+    public function respuestaMultipleActividad(){
+        return $this->hasMany('App/RespuestaMultipleActividad','Id_Pregunta');
+    }
+
+    public function respuestaAbiertaActividad(){
+        return $this->hasMany('App/RespuestaAbiertaActividad','Id_Pregunta');
+    }
+
+}
