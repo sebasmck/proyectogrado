d385193020104a2bf85383c97e719fd98f20ddff
sebastian Farias <corporate.att@gmail.com>
2018-10-14 14:55:26 -0500

    cnr beta 1

--- resilient/resources/views/activities/2-3anos/CulminadoActividadCnr.blade.php
+++ resilient/resources/views/activities/2-3anos/CulminadoActividadCnr.blade.php
@@ -1,110 +1,138 @@
 @extends('partials.layout')
 @section('title')
-Culminación 
+Culminación
 @endsection
 
 @section('addcss')
 
 <style>
 
-body {
-  background: #0C5AF0;
-  padding: 40px;
-  color: rgba(0, 0, 0, 0.6);
-  font-family: 'Roboto', sans-serif;
-  font-size: 14px;
-  line-height: 1.7em;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-body:before {
-  content: '';
-  position: absolute;
-  background: #30343E;
-  width: 80%;
-  height: 200%;
-  box-shadow: 0 0 60px rgba(0, 0, 0, 0.3);
-  border-radius: 100%;
-  -webkit-transform: translate(-20%, -40%) rotate(30deg);
-          transform: translate(-20%, -40%) rotate(30deg);
-}
+@import url('https://fonts.googleapis.com/css?family=Indie+Flower');
+@import url('https://fonts.googleapis.com/css?family=Amatic+SC');
 
-::-moz-selection {
-  background: #E84F89;
-}
+/* body {
+	font-family: 'Indie Flower', cursive !important;
+  background: #FDE3A7; /*CAPE HONEY*/
+	margin: 0px;
+	padding: 0px;
+} */
 
 ::selection {
-  background: #E84F89;
+	background: transparent;
 }
 
-.modal {
-  z-index: 1000;
-  position: relative;
-  background: #FFF;
-  max-width: 530px;
-  border-radius: 6px;
-  box-shadow: 0 0 30px rgba(0, 0, 0, 0.2);
-  box-sizing: border-box;
-  margin: 0 auto;
-  padding: 40px 60px 0;
-  overflow: hidden;
-  -webkit-transform-style: preserve-3d;
-          transform-style: preserve-3d;
-  -webkit-perspective: 100;
-          perspective: 100;
-}
-.modal.toggled {
-  -webkit-animation: flip 3s ease;
-          animation: flip 3s ease;
+h4 {
+	font-size: 26px;
+	line-height: 1px;
+	font-family: 'Amatic SC', cursive !important;
 }
 
-.trophy {
-  display: block;
-  margin: 0 auto 40px;
+.color1{color:#1BBC9B}/*MOUNTAIN MEADOW*/
+.color2{color:#C0392B/*TALL POPPY*/}
+
+
+.card {
+	color: #013243; /*SHERPA BLUE*/
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	width: 300px;
+	height: 400px;
+	background: #e0e1dc;
+	transform-style: preserve-3d;
+	transform: translate(-50%,-50%) perspective(2000px);
+	box-shadow: inset 300px 0 50px rgba(0,0,0,.5), 20px 0 60px rgba(0,0,0,.5);
+	transition: 1s;
 }
 
-h1 {
-  display: block;
-  margin: 0 0 20px;
-  color: rgba(0, 0, 0, 0.8);
-  font-size: 24px;
-  font-weight: 400;
-  text-align: center;
+.card:hover {
+	transform: translate(-50%,-50%) perspective(2000px) rotate(15deg) scale(1.2);
+	box-shadow: inset 20px 0 50px rgba(0,0,0,.5), 0 10px 100px rgba(0,0,0,.5);
 }
 
-p {
-  margin: 0 0 40px;
-  padding: 0 60px;
-  font-size: 18px;
-  text-align: center;
+.card:before {
+	content:'';
+	position: absolute;
+	top: -5px;
+	left: 0;
+	width: 100%;
+	height: 5px;
+	background: #BAC1BA;
+	transform-origin: bottom;
+	transform: skewX(-45deg);
 }
 
-button {
-  outline: 0;
-  background: linear-gradient(#5388C5, #2A78DD);
-  width: 35%;
-  border: 0;
-  border-radius: 100px;
-  box-shadow: 0 5px 10px rgba(42, 120, 221, 0.3);
-  margin: 0 15px;
-  padding: 15px;
-  box-sizing: border-box;
-  color: #FFF;
-  font-size: 16px;
-  font-weight: 600;
-  cursor: pointer;
-}
-button:last-child {
-  background: linear-gradient(#54CE42, #1FCD37);
-  box-shadow: 0 5px 10px rgba(31, 205, 55, 0.3);
+.card:after {
+	content: '';
+	position: absolute;
+	top: 0;
+	right: -5px;
+	width: 5px;
+	height: 100%;
+	background: #92A29C;
+	transform-origin: left;
+	transform: skewY(-45deg);
 }
 
-footer {
-  background: #FFF;
-  box-shadow: 0 0 15px rgba(0, 0, 0, 0.06);
-  margin: 0 -60px;
-  padding: 30px 0;
-  text-align: center;
+.card .imgBox {
+	width: 100%;
+	height: 100%;
+	position: relative;
+	transform-origin: left;
+	transition: .7s;
+}
+
+.card .bark {
+	position: absolute;
+	background: #e0e1dc;
+	width: 100%;
+	height: 100%;
+	opacity: 0;
+	transition: .7s;
+}
+
+.card .imgBox img {
+	min-width: 250px;
+	max-height: 400px;
+}
+
+.card:hover .imgBox {
+	transform: rotateY(-135deg);
+}
+
+.card:hover .bark {
+  font-family: 'Indie Flower', cursive !important;
+	opacity: 1;
+	transition: .6s;
+  box-shadow: 300px 200px 100px rgba(0, 0, 0, .4) inset;
+}
+
+.card .details {
+  font-family: 'Indie Flower', cursive !important;
+	position: absolute;
+	top: 0;
+	left: 0;
+	box-sizing: border-box;
+	padding: 0 0 0 10px;
+	z-index: -1;
+	margin-top: 70px;
+}
+
+.card .details p {
+  font-family: 'Indie Flower', cursive !important;
+	font-size: 15px;
+	line-height: 5px;
+	transform: rotate(-10deg);
+	padding: 0 0 0 20px;
+}
+
+.card .details h4 {
+  font-family: 'Indie Flower', cursive !important;
+	text-align: center;
+}
+
+.text-right {
+	text-align: right;
 }
 
 </style>
@@ -112,55 +140,50 @@ footer {
 @endsection
 
 @section('content')
-<div class="col-lg-offset-2 col-md-8">
-        <form class="form">
-        {{csrf_field()}}
-            <div class="card">
 
-                <div class="card-head style-primary">
-                   <header> Practica Sentido Del Humor </header>
-                </div>
-                <div class="card-body floating-label">
-                    
-                    <!-- animation and stuff -->
 
-                    <div class="modal"><img class="trophy" src="{{asset('img/trophy.png')}}"/>
-                        <h1>Congratulations</h1>
-                        <p>You have successfully completed the 100 Days UI Challenge</p>
-                        <footer>
-                          <button>Share</button>
-                          <button>Have a beer</button>
-                        </footer>
-                      </div>
+<div class="card">
+		<div class="imgBox">
+			<div class="bark"></div>
+			<img src="{{asset('img/congrats.png')}}">
+		</div>
+		<div class="details">
+			<!-- <h4 class="color1">You're not a Fossil! (YET)</h2> -->
+      <h4 class="color2 margin">Felicitaciones!!</h3>
+        <br><br>
 
-                </div> 
-            </div><!--end .card-body -->
-            <div class="card-actionbar">
-                  <div class="card-actionbar-row">
-                  <a style="btn btn-flat btn-primary ink-reaction" href=""> <button type="button" class="btn btn-default ink-reaction btn-primary-dark">Finalizar</button></a>
-                  </div>
-            </div>
-
-        </div><!--end .card -->
-            
-        </form>
-    </div>
+      <p>Lo lograste!</p>
+      <br>
+      <br>
+			<p>Da clic en el boton siguiente</p>
+			<p>Para continuar</p>
+      <br>
+      <br>
+      <p><div class="card-actionbar">
+          <div class="card-actionbar-row">
+          <a style="btn btn-flat btn-primary ink-reaction" href="{{route('/cnr2')}}"> <button type="button" class="btn btn-default ink-reaction btn-primary-dark">Siguiente</button></a>
+          </div>
+    </div></p>
+		</div>
+  </div>
+  
+  
 
 @endsection
- @section('addjs')
+@section('addjs')
 
-        <script scr="https://code.jquery.com/jquery-1.12.4.js"></script>
-        <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-		<script>
-			$(document).ready(function() {
-				$('#example').DataTable();
-			} );
-		</script>
-        <script>
-            $(document).ready( function () {
-                $('#example').DataTable();
-            } );    
-        </script>
+<script scr="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script>
+  $(document).ready(function () {
+    $('#example').DataTable();
+  });
+</script>
+<script>
+  $(document).ready(function () {
+    $('#example').DataTable();
+  });    
+</script>
 
 
-@endsection  
+@endsection
