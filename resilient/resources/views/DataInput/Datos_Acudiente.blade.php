f14c76f2e5b22fe6d749551cdad569fc8cf0774d
sebastian Farias <corporate.att@gmail.com>
2018-10-27 00:56:04 -0500

    update on dropdownbox

--- resilient/resources/views/DataInput/Datos_Acudiente.blade.php
+++ resilient/resources/views/DataInput/Datos_Acudiente.blade.php
@@ -4,9 +4,72 @@
 Datos Acudiente
 @endsection
 
+@section('addcss')
+<script type="text/javascript">
+    //Applies cascading behavior for the specified dropdowns 
+    function applyCascadingDropdown(sourceId, targetId) { 
+      var source = document.getElementById(sourceId); 
+      var target = document.getElementById(targetId); 
+      if (source && target) { 
+        source.onchange = function() { 
+          displayOptionItemsByClass(target, source.value); 
+        } 
+        displayOptionItemsByClass(target, source.value); 
+      } 
+    } 
+    
+    //hides dropdown boxes
+    function ChangeDropdowns(value){
+      if(value=="1"){
+        document.getElementById('stuff').style.display='none';
+      }else if(value=="4"){
+        document.getElementById('stuff').style.display='block';
+      }
+    }
+    
+    //Displays a subset of a dropdown's options 
+    function displayOptionItemsByClass(selectElement, className) { 
+      if (!selectElement.backup) { 
+        selectElement.backup = selectElement.cloneNode(true); 
+      } 
+      var options = selectElement.getElementsByTagName("option"); 
+      for(var i=0, length=options.length; i<length; i++) { 
+        selectElement.removeChild(options[0]); 
+      } 
+      var options = selectElement.backup.getElementsByTagName("option"); 
+      for(var i=0, length=options.length; i<length; i++) { 
+        if (options[i].className==className) 
+          selectElement.appendChild(options[i].cloneNode(true)); 
+      } 
+    } 
+    
+    //Binds dropdowns 
+    function applyCascadingDropdowns() { 
+      applyCascadingDropdown("categories", "items"); 
+      applyCascadingDropdown("items", "stuff");
+    //We could even bind items to another dropdown 
+    //applyCascadingDropdown("items", "foo"); 
+    } 
+    
+    function combine(info) {
+      var str = '';
+      str += document.getElementById('categories').value
+           + ' ' + document.getElementById('items').value
+           + ' ' + document.getElementById('stuff').value
+           + ' ' + info;
+      alert(str);
+    }
+    
+    //execute when the page is ready 
+    window.onload=applyCascadingDropdowns;
+    
+    </script>
+
+
+
+@endsection
+
 @section('content')
-
-
 <div class="col-lg-offset-2 col-md-8">
     <form action="{{route('cuidador.store')}}" method="POST" class="form">
 
@@ -162,50 +225,41 @@ Datos Acudiente
                 <div class="row">
                         <div class="col-sm-6">
                                 <div class="form-group">
-                                        <select id="Id_Departamento" name="Id_Departamento" class="form-control">
-                                               <option value="">&nbsp;</option>
-                                               <option value="30">Amazonas</option>
-                                               <option value="40">Antioquia</option>
-                                               <option value="40">Arauca</option>
-                                               <option value="40">Atlantico</option>
-                                               <option value="40">Bogota D.c</option>
-                                               <option value="40">Bolivar</option>
-                                               <option value="40">Boyacá</option>
-                                               <option value="40">Caldas</option>
-                                               <option value="40">Caquetá</option>
-                                               <option value="40">Casanare</option>
-                                               <option value="40">Cauca</option>
-                                               <option value="40">Cesar</option>
-                                               <option value="40">Choco</option>
-                                               <option value="40">Cordoba</option>
-                                               <option value="40">Cundinamarca</option>
-                                               <option value="40">Guainia</option>
-                                               <option value="40">Huila</option>
-                                               <option value="40">La Guajira</option>
-                                               <option value="40">Magdalena</option>
-                                               <option value="40">Meta</option>
-                                               <option value="40">Nariño</option>
-                                               <option value="40">Vaupes</option>
-                                               <option value="40">Putumayo</option>
-                                               <option value="40">Quindio</option>
-                                               <option value="40">Risaralda</option>
-                                               <option value="40">Vichada</option>
-                                               <option value="40">Santander</option>
-                                               <option value="40">Sucre</option>
-                                               <option value="40">Tolima</option>
-                                               <option value="40">Valle del Cauca</option>
-                                               <option value="40">Norte de Santander</option>
-                                               <option value="40">San Andres y Providencia</option>
-                                        </select>
+                                        <select  id="categories" name="categories" onchange="document.theform.showValue.value=this.value" class="form-control"> 
+                                                <option value="">&nbsp;</option>
+                                                <Option Value="Agriculture">Cundinamarca </Option>
+                                                <Option Value="Apparel & Accessories">Magdalena</Option>
+                                                <Option Value="Automobile">Santander</Option>
+        
+                                        </select> 
                                         <label for="select1">Departamento</label>
                                    </div>
                         </div>
                         <div class="col-sm-6">
                                 <div class="form-group">
-                                        <select id="Id_Ciudad" name="Id_Ciudad" class="form-control">
-                                               <option value="">&nbsp;</option>
-                                               <option value="30"></option>
-                                        </select>
+                                        <select name="items" id="items" class="form-control"> 			
+                                                <option value="">&nbsp;</option>
+                                                <Option Class="Agriculture" Value="Bogota">Bogota</Option> 
+                                                <Option Class="Agriculture" Value="Soacha">Soacha</Option> 
+                                                <Option Class="Agriculture" Value="Chia">Chia</Option> 
+                                                <Option Class="Agriculture" Value="Facatativá">Facatativá</Option> 
+               
+                                                
+                                                <option value="">&nbsp;</option>
+                                                <Option Class="Apparel & Accessories" Value="Ciénaga">Ciénaga</Option> 
+                                                <Option Class="Apparel & Accessories" Value="StaMarta">Sta Marta</Option>
+                                                <Option Class="Apparel & Accessories" Value="ZonaBananera">Zona Bananera</Option>
+                                                <Option Class="Apparel & Accessories" Value="plato">plato</Option>
+               
+                                                
+                                                
+                                                <option value="">&nbsp;</option>
+                                                <Option Class="Automobile" Value="Automobile">Piedecuesta</Option> 
+                                                <Option Class="Automobile" Value="Girón">Girón</Option>
+                                                <Option Class="Automobile" Value="Cúcuta">Cúcuta</Option>
+                                                <Option Class="Automobile" Value="Bucaramanga">Bucaramanga</Option>
+               
+                                           </select>
                                         <label for="select1">Ciudad</label>
                                    </div>
                         </div>
